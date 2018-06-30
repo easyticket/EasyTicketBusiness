3e1eda219cf9d6a847ad7a6670614cb3f068176c
diegoesteban2a <deaa1a@hotmail.com>
2018-06-30 12:26:34 -0500

    add EventTicketController

--- src/main/java/com/dise/tickets/EasyTicketApplication.java
+++ src/main/java/com/dise/tickets/EasyTicketApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasyTicketApplication {
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EasyTicketApplication.class, args);
 	}
