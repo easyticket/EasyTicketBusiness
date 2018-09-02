716e3bcd4f81b563a34158623968e6b68a38e06a
juansefo <sebastianforero04@outlook.com>
2018-09-02 08:53:01 -0500

    jwt

---
+++ src/main/java/com/dise/tickets/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,99 @@
+package com.dise.tickets.auth.filter;
+
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+	private AuthenticationManager autenticationManager;
+	
+	
+	
+	public JWTAuthenticationFilter(AuthenticationManager autenticationManager) {
+		this.autenticationManager = autenticationManager;
+		setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/v1/login","POST"));
+	}
+
+
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		String username = obtainUsername(request);
+		String password = obtainPassword(request);
+
+		if (username == null) {
+			username = "";
+		}
+
+		if (password == null) {
+			password = "";
+		}
+		
+		if (username!=null&&password!=null) {
+			logger.info("User desde form-data "+username);
+			logger.info("Pass desde form-data "+password);
+
+		}
+		
+		username = username.trim();
+		
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+		return autenticationManager.authenticate(authToken);
+	}
+
+
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		KeyPairGenerator kpg;
+		KeyPair kp =null;
+		try {
+			kpg = KeyPairGenerator.getInstance("RSA");
+			kp= kpg.generateKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String token = Jwts.builder()
+				.setSubject(username)
+				.signWith(kp.getPrivate())
+				.compact();
+		response.addHeader("Authorization", "Bearer "+token);
+		
+		Map<String, Object> body = new HashMap<>();
+		
+		body.put("token",token);
+		body.put("user",(User) authResult.getPrincipal());
+		body.put("mensaje","Hola perra");
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("aplication/json");
+	}
+	
+	
+
+}
---
+++ src/main/java/com/dise/tickets/entity/UserTicket.java
@@ -0,0 +1,117 @@
+package com.dise.tickets.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_ticket")
+public class UserTicket implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6753849588006962772L;
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "username", unique= true)
+	private String username;
+	
+	@Column(name = "pass")
+	private String pass;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_authority", insertable = false, updatable = false ,foreignKey = @ForeignKey(name = "fk_authority"))
+	private Role role;
+
+	
+	public UserTicket() {
+		super();
+	}
+
+	public UserTicket(String username, String pass, String address, String email) {
+		super();
+		this.username = username;
+		this.pass = pass;
+		this.address = address;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+}
